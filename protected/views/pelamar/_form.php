b00c4db55620415f46642f16e700796ebe50dc2c
Muhamad Rizal Indrabayu <indrabayu.rizal@gmail.com>
2014-05-16 13:35:43 +0700

    merged

--- protected/views/pelamar/_form.php
+++ protected/views/pelamar/_form.php
@@ -11,7 +11,7 @@
 		echo '<div class="flash-' . $key . '">' . $message . "</div>\n";
 	}
 	?>
-	
+
 	<?php $form=$this->beginWidget('CActiveForm', array(
 		'id'=>'pelamar-form',
 	// Please note: When you enable ajax validation, make sure the corresponding
