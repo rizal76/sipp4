4ea62f350c21139c716555e7eaf75dd04049115c
Muhamad Rizal Indrabayu <indrabayu.rizal@gmail.com>
2014-05-17 17:16:42 +0700

    beres2

--- protected/views/pelamar/admin.php
+++ protected/views/pelamar/admin.php
@@ -18,7 +18,7 @@ $('.search-form form').submit(function(){
 
 <h1>Daftar Pelamar</h1>
 
-<?php 
+<?php
 //
 //$this->widget('application.extensions.tablesorter.Sorter', array(
 //    'data'=>$model,
@@ -34,34 +34,40 @@ $('.search-form form').submit(function(){
 //		'cv',// Relation value given in model
 //    )
 //));
+$link = Yii::app()->baseUrl.'/cv/';
+$this->widget('zii.widgets.grid.CGridView', array(
+'id' => 'pelamar-grid',
+ 'dataProvider' => $model->search(),
+ 'filter' => $model,
+ 'columns' => array(
+'nama',
+ 'kota',
+ 'umur',
+ 'jenis_kelamin',
+ 'gaji',
+ 'pendidikan',
+ array('name'=>'cv',
+     'type'=>'raw',
+    'value' => 'CHtml::link( "CV", "cv/".$data->cv)',
 
- $this->widget('zii.widgets.grid.CGridView', array(
- 	'id'=>'pelamar-grid',
- 	'dataProvider'=>$model->search(),
- 	'filter'=>$model,
- 	'columns'=>array(
- 		'id',
- 		'no_ktp',
- 		'nama',
- 		'tempat_lahir',
- 		'umur',
- 		'jenis_kelamin',
- 		/*
- 		'status',
- 		'jumlah_anak',
- 		'alamat',
- 		'kota',
- 		'tlp',
- 		'pendidikan',
- 		'jenjang',
- 		'jurusan',
- 		'tahun_lulus',
- 		'skill',
- 		'gaji',
- 		'cv',
- 		*/
- 		array(
- 			'class'=>'CButtonColumn',
- 		),
- 	),
- )); ?>
+     ),
+/*
+  'status',
+  'jumlah_anak',
+  'alamat',
+  'kota',
+  'tlp',
+  'pendidikan',
+  'jenjang',
+  'jurusan',
+  'tahun_lulus',
+  'skill',
+  'gaji',
+  'cv',
+ */
+array(
+'class' => 'CButtonColumn',
+ ),
+ ),
+));
+?>
