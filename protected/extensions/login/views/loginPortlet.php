0d94ffb90070b55795b228e9f3c10b117eb406ea
Muhamad Rizal Indrabayu <indrabayu.rizal@gmail.com>
2014-05-19 07:31:36 +0700

    css login error bug fixed

--- protected/extensions/login/views/loginPortlet.php
+++ protected/extensions/login/views/loginPortlet.php
@@ -1,13 +1,13 @@
 
 <?php echo CHtml::form();
 ?>
-<?php echo CHtml::activeEmailField($user,'username',array('size'=>60,'maxlength'=>255,'encode'=>false,'value'=>'','placeholder'=>'username')) ?>
-<?php echo CHtml::error($user,'username', array('class'=>'alert-danger')); ?>
+<?php echo CHtml::activeEmailField($user,'username',array('size'=>60,'maxlength'=>255,'encode'=>false,'value'=>'','placeholder'=>'username/email')) ?>
+<?php echo CHtml::error($user,'username', array('class'=>'alert-danger login-salah')); ?>
 
 
 
 <?php echo CHtml::activePasswordField($user,'password',array('size'=>60,'maxlength'=>255,'encode'=>false,'value'=>'','placeholder'=>'password')) ?>
-<?php echo CHtml::error($user,'password', array('class'=>'alert-danger')); ?>
+<?php echo CHtml::error($user,'password', array('class'=>'alert-danger login-salah')); ?>
 
 <p class="login button"> 
 	<input type="submit" value="Login" />
