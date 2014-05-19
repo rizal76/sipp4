7d0266fd134350565444be9c6210fe7cda0bb5e5
Muhamad Rizal Indrabayu <indrabayu.rizal@gmail.com>
2014-05-19 18:29:48 +0700

    change accesrule for pengumuman

--- protected/controllers/LamaranController.php
+++ protected/controllers/LamaranController.php
@@ -31,7 +31,7 @@ class LamaranController extends Controller {
 //			),
             array('allow', // allow authenticated user to perform 'create' and 'update' actions
                 'actions' => array('pengumuman'),
-                'users' => array('@'),
+                'users' => array('$user->isSuperMember()'),
             ),
 //			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 //				'actions'=>array('admin','delete'),
